bbb773f633e513b2ea9c90302a1cc3324ba2ea6f
beilunyang <786220806@qq.com>
2015-09-30 05:54:28 -0700

    basic app

--- app.js
+++ app.js
@@ -1,18 +1,56 @@
 var express = require('express');
+var swig = require('swig');
 var app = express();
-//var server = require('http').Server(app);
-//var io = require('socket.io')(server);
 var server = app.listen(3000,function(){
 	console.log('服务器已经启动。。。');
 });
 var io =require('socket.io')(server);
+app.use(require('body-parser')());
 app.use(express.static(__dirname+'/public'));
+app.engine('html',swig.renderFile);
+app.set('view engine','html');
+app.set('views',__dirname+'/views');
+
+var sessionMiddleware = require('express-session')({secret:'3ddfse47i'});
+
+app.use(sessionMiddleware);
+
 app.get('/',function(req,res){
-	res.sendFile(__dirname+'/views/index.html');
+	if(!req.session.info){
+		res.render('index');	
+	}else{
+		res.render('index',{form:'hh'});
+	}
 });
-io.on('connection',function(socket){
-	socket.on('chat message',function(msg){
-		io.emit('chat message',msg);
-	});
+app.post('/login',function(req,res){
+	if(req.body.niname){
+		req.session.info = {
+			niname : req.body.niname
+		}
+		res.redirect(303,'/');
+	}else{
+		res.redirect(303,'/');
+	}
 });
 
+io.use(function(socket,next){
+	sessionMiddleware(socket.request,socket.request.res,next);
+	next(new Error('a error'));
+});
+io.on('connection',function(socket){
+	var session = socket.request.session;
+	try{
+		var niname = session.info.niname;
+	}catch(e){
+		var niname = '匿名';
+	}
+	socket.on('chat message',function(msg){
+		io.emit('chat message',niname,msg);
+	});	
+});
+app.use(function(error,req,res,next){
+	if(error) console.stack(error);
+	else{
+		res.send(404,'have a error');
+	}
+});
--- public/script.js
+++ public/script.js
@@ -4,8 +4,8 @@ $('#shuru').submit(function(){
 	$('#messages').val('');
 	return false;
 });
-socket.on('chat message',function(msg){
-	var apHtml = '<div><div class="dialog"><div class="name">悖论</div><div class="info">'+msg+'</div></div><div style="clear: both;"></div></div>';
+socket.on('chat message',function(niname,msg){
+	var apHtml = '<div><div class="dialog"><div class="name">'+niname+'</div><div class="info">'+msg+'</div></div><div style="clear: both;"></div></div>';
 	$('#mCSB_1_container').append($(apHtml));
 	$('.room').mCustomScrollbar('scrollTo','bottom');
 })
